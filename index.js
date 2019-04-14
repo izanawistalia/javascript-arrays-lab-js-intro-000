df6c16c167607611d04efd0b9e3be548f1e71d28
rihan <dikshabelwal0@gmail.com>
2019-04-14 04:34:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-var kittens = [] //define your array here
-
-// Add your functions and code here
+var kittens = ['Milo','Otis','Garfield']; //define your array here
+function destructivelyAppendKitten(){
+  kittens.push('Ralph');
+}
+function destructivelyPrependKitten(){
+  kittens.shift('Ralph');
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.unshift();
+}
+function appendKitten(name){
+  return (var narr = kittens.concat(name))
+}
+function prependKitten(name){
+  var narr = [...kittens, name]
+  return narr;
+}
